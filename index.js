1ff60f029710f83e2edde2b2ef10ccaa4fc1a7a0
Kevin Beaty <kevin@simplectic.com>
2013-03-05 22:29:33 -0600

    extract functions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var collections = require('./lib/collections')
   , arrays = require('./lib/arrays')
   , objects = require('./lib/objects')
+  , functions = require('./lib/functions')
   , when = require('when')
   , through = require('through')
   , u = require('./lib/util')
@@ -84,87 +85,13 @@ function pipe(producer, write, options){
         });
 }
 
-_r.tap = tap
-function tap(producer, doNext, doComplete, doError){
-  return produce(
-      producer
-    , null
-    , function(consumer, obj){
-        if(isFunction(doNext)) doNext(obj)
-        consumer.next(obj)
-      }
-    , function(consumer){
-        if(isFunction(doComplete)) doComplete()
-        consumer.complete()
-      }
-    , function(consumer, error){
-        if(isFunction(doError)) doError(error)
-        consumer.error(error)
-      })
-}
-
-function delayConsumer(action, delayFun, arg){
-  return function(consumer, val){
-    var fun = function(){
-      consumer[action](val)
-    }
-    delayFun(fun, arg)
-  }
-}
-
-function produceDelayConsumer(producer, delayFun, arg){
-  var args = _slice.call(arguments, 2)
-  return produce(
-      producer
-    , null
-    , delayConsumer('next', delayFun, arg)
-    , delayConsumer('complete', delayFun, arg)
-    , delayConsumer('error', delayFun, arg))
-}
-
-_r.delay = delay
-function delay(producer, wait){
-  return produceDelayConsumer(producer, setTimeout, wait)
-}
-
-_r.defer = defer
-function defer(producer){
-  return produceDelayConsumer(producer, _nextTick)
-}
-
-_r.debounce = debounce
-function debounce(producer, wait, immediate){
-  var timeout = null
-    , sendIt = identity
-  return produce(
-      producer
-    , null
-    , function(consumer, obj){
-        var callNow = immediate && !timeout
-        sendIt = function(){
-            timeout = null
-            if(!immediate) consumer.next(obj)
-          }
-        clearTimeout(timeout)
-        timeout = setTimeout(sendIt, wait)
-        if(callNow) consumer.next(obj)
-      }
-    , function(consumer){
-        clearTimeout(timeout)
-        sendIt()
-        consumer.complete()
-      }
-    , function(consumer, error){
-        clearTimeout(timeout)
-        consumer.error(error)
-      })
-}
 
 _r.mixin = Underarm.mixin
 _r.mixin(_r)
 _r.mixin(collections)
 _r.mixin(arrays)
 _r.mixin(objects)
+_r.mixin(functions)
 
 _r.chain = Underarm.chain
 _r.when = when
